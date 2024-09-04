7e02dc918d6aff4a7bb88b5d0c211dcf7f2d475b
Gayu <gayuvick2002@gmail.com>
2024-09-04 13:52:35 +0530

    add vercel config

--- functions/get-tasks.js
+++
@@ -1,9 +0,0 @@
-const tasks = require('./tasks');
-  
-  exports.handler = async function(event, context) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ data: tasks }),
-    };
-  };
-  
