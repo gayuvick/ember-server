a300270a435f0f2a251de1e9939c7fc1dfcc0243
Gayu <gayuvick2002@gmail.com>
2024-09-04 14:39:28 +0530

    chnage id to string

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use(cors()); // Enable CORS if needed
 
 let tasks = [
-  { id: 1, type:'task', attributes:{description: 'Task 1', dueDate: '2024-09-01T00:00:00.000Z', completed: false }},
-  { id: 2, type:'task', attributes:{description: 'Task 2', dueDate: '2024-09-10T00:00:00.000Z', completed: true} }
+  { id: '1', type:'task', attributes:{description: 'Task 1', dueDate: '2024-09-01T00:00:00.000Z', completed: false }},
+  { id: '2', type:'task', attributes:{description: 'Task 2', dueDate: '2024-09-10T00:00:00.000Z', completed: true} }
 ];
 console.log('app running')
 // GET all tasks
