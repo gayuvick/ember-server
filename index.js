9f5bf00d75997ba43a4a2be922d7aa5c8163850f
Gayu <gayuvick2002@gmail.com>
2024-09-05 01:09:37 +0530

    update PUT request

--- index.js
+++ index.js
@@ -10,13 +10,12 @@ app.use(express.json());
 app.use(cors()); // Enable CORS if needed
 
 let tasks = [
-  { id: '1', type:'task', attributes:{description: 'Task 1', dueDate: '2024-09-01T00:00:00.000Z', completed: false }},
-  { id: '2', type:'task', attributes:{description: 'Task 2', dueDate: '2024-09-10T00:00:00.000Z', completed: true} }
+  { id: 1, type:'task', attributes:{description: 'Task 1', dueDate: '2024-09-01T00:00:00.000Z', completed: false }},
+  { id: 2, type:'task', attributes:{description: 'Task 2', dueDate: '2024-09-10T00:00:00.000Z', completed: true} }
 ];
 console.log('app running')
 // GET all tasks
 app.get('/api/tasks', (req, res) => {
-console.log('coming to get request');
   res.json({ data: tasks });
 
 });
@@ -34,7 +33,6 @@ app.get('/api/tasks/:id', (req, res) => {
 
 // POST a new task
 app.post('/api/tasks', (req, res) => {
-    console.log("coming for post?")
   const newTask = req.body;
   newTask.id = tasks.length + 1; // Simple ID assignment logic
   tasks.push(newTask);
@@ -53,7 +51,7 @@ app.put('/api/tasks/:id', (req, res) => {
   const taskId = parseInt(req.params.id, 10);
   const updatedTask = req.body;
   tasks = tasks.map(task => (task.id === taskId ? updatedTask : task));
-  res.json({ data: updatedTask });
+  res.json({ data: tasks });
 });
 
 app.listen(port, () => {
